6902bb280184ab325b36845207d1bf74b1ec5659
tuguzT <timurka.tugushev@gmail.com>
2021-12-28 13:33:02 +0300

    Move `AddSet` trait in separate module

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -1,3 +1,4 @@
+mod add_set;
 pub mod registry;
 
 slotmap::new_key_type! {
--- src/entity/registry.rs
+++ src/entity/registry.rs
@@ -6,6 +6,8 @@ use slotmap::SlotMap;
 use crate::component::pool::ComponentPool;
 use crate::{Component, Entity};
 
+use super::add_set::AddSet;
+
 pub struct Registry {
     entities: SlotMap<Entity, ()>,
     pools: HashMap<TypeId, Box<dyn Any>>,
@@ -101,35 +103,4 @@ impl Registry {
         let pool = pool.as_mut().downcast_mut().expect("downcast error");
         Some(pool)
     }
-}
-
-pub trait AddSet {
-    fn add_set(self, registry: &mut Registry, entity: Entity);
-}
-
-mod impls {
-    use super::*;
-
-    macro_rules! add_set_impl {
-        ($arg:ident | $count:tt) => ();
-
-        ($arg:ident $(, $args:ident)+ | $idx:tt $(, $idxs:tt)+) => {
-            impl<$arg$(, $args)+> AddSet for ($arg$(, $args)+)
-            where
-                $arg: Component$(,
-                $args: Component
-                )+,
-            {
-                fn add_set(self, registry: &mut Registry, entity: Entity) {
-                    registry.add(entity, self.$idx);
-                    $(registry.add(entity, self.$idxs);)*
-                }
-            }
-
-            add_set_impl!($($args),+ | $($idxs),+);
-        };
-    }
-
-    #[rustfmt::skip]
-    add_set_impl!(L, K, J, I, H, G, F, E, D, C, B, A | 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
 }
