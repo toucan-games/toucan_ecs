c5ef0695832cdeec40d2070e372c8e6038fcfffc
tuguzT <timurka.tugushev@gmail.com>
2022-02-04 17:08:29 +0300

    Rearrange associated functions

--- src/entity/entry.rs
+++ src/entity/entry.rs
@@ -20,6 +20,38 @@ impl<'r> Entry<'r> {
         Entry { entity, registry }
     }
 
+    /// Destroys the entity and removes all its attached components.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use toucan_ecs::{Registry, Entry};
+    /// # let mut registry = Registry::new();
+    /// let mut entry = registry.create_entry();
+    /// let entity = entry.entity();
+    ///
+    /// entry.destroy();
+    /// assert!(!registry.contains(entity));
+    /// ```
+    pub fn destroy(self) {
+        self.registry.destroy(self.entity)
+    }
+
+    /// Returns unique handle of the entity.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use toucan_ecs::{Registry, Entry};
+    /// # let mut registry = Registry::new();
+    /// let mut entry = registry.create_entry();
+    /// let entity = entry.entity();
+    /// assert!(registry.contains(entity));
+    /// ```
+    pub fn entity(&self) -> Entity {
+        self.entity
+    }
+
     /// Attaches exactly one component to the entity.
     ///
     /// This function does not panic because it registers component type automatically.
@@ -75,60 +107,59 @@ impl<'r> Entry<'r> {
         self.registry.attach(self.entity, set)
     }
 
-    /// Retrieves the [shared borrow][`Ref`] for the component of one type attached to the entity.
-    /// Returns [`None`][`Option::None`] if component is not attached to the entity.
+    /// Returns `true` if component of generic type is attached to the entity.
     ///
-    /// Note that function would block current thread
-    /// if the same instance of component will be retrieved more than once.
+    /// To check if the entity has components of multiple types,
+    /// use [`attached`][`Entry::attached`] associated function.
     ///
     /// # Examples
     ///
     /// ```
     /// # use toucan_ecs::{Registry, Entry};
     /// # let mut registry = Registry::new();
-    /// #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    /// #[derive(Copy, Clone)]
     /// struct Name(&'static str);
     ///
     /// let mut entry = registry.create_entry();
-    /// entry.attach_one(Name("Hello, World"));
+    /// assert!(!entry.attached_one::<Name>());
     ///
-    /// let name = entry.get::<Name>().unwrap();
-    /// assert_eq!(*name, Name("Hello, World"));
+    /// entry.attach_one(Name("Hello, World"));
+    /// assert!(entry.attached_one::<Name>());
     /// ```
-    pub fn get<C>(&'r self) -> Option<Ref<'r, C>>
+    pub fn attached_one<C>(&self) -> bool
     where
         C: Component,
     {
-        self.registry.get(self.entity)
+        self.registry.attached_one::<C>(self.entity)
     }
 
-    /// Retrieves the [unique borrow][`RefMut`] for the component of one type attached to the entity.
-    /// Returns [`None`][`Option::None`] if component is not attached to the entity.
+    /// Returns `true` if components in the generic set type are attached to the entity.
     ///
-    /// Note that function would block current thread
-    /// if the same instance of component will be retrieved more than once.
+    /// To check if the entity has component of exactly one type,
+    /// use [`attached_one`][`Entry::attached_one`] associated function.
     ///
     /// # Examples
     ///
     /// ```
     /// # use toucan_ecs::{Registry, Entry};
     /// # let mut registry = Registry::new();
-    /// #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    /// #[derive(Copy, Clone)]
     /// struct Name(&'static str);
     ///
-    /// let mut entry = registry.create_entry();
-    /// entry.attach_one(Name("Hello, World"));
+    /// #[derive(Copy, Clone)]
+    /// struct ID(u32);
     ///
-    /// let mut name = entry.get_mut::<Name>().unwrap();
-    /// (*name).0 = "This name was changed";
-    /// assert_ne!(*name, Name("Hello, World"));
-    /// assert_eq!(*name, Name("This name was changed"));
+    /// let mut entry = registry.create_entry();
+    /// assert!(!entry.attached::<(ID, Name)>());
+    ///
+    /// entry.attach((Name("Hello, World"), ID(42)));
+    /// assert!(entry.attached::<(Name, ID)>());
     /// ```
-    pub fn get_mut<C>(&'r mut self) -> Option<RefMut<'r, C>>
+    pub fn attached<S>(&self) -> bool
     where
-        C: Component,
+        S: ComponentSet,
     {
-        self.registry.get_mut(self.entity)
+        self.registry.attached::<S>(self.entity)
     }
 
     /// Removes component of one type from the entity.
@@ -216,90 +247,59 @@ impl<'r> Entry<'r> {
         self.registry.remove_all(self.entity)
     }
 
-    /// Returns `true` if component of generic type is attached to the entity.
+    /// Retrieves the [shared borrow][`Ref`] for the component of one type attached to the entity.
+    /// Returns [`None`][`Option::None`] if component is not attached to the entity.
     ///
-    /// To check if the entity has components of multiple types,
-    /// use [`attached`][`Entry::attached`] associated function.
+    /// Note that function would block current thread
+    /// if the same instance of component will be retrieved more than once.
     ///
     /// # Examples
     ///
     /// ```
     /// # use toucan_ecs::{Registry, Entry};
     /// # let mut registry = Registry::new();
-    /// #[derive(Copy, Clone)]
+    /// #[derive(Copy, Clone, Eq, PartialEq, Debug)]
     /// struct Name(&'static str);
     ///
     /// let mut entry = registry.create_entry();
-    /// assert!(!entry.attached_one::<Name>());
-    ///
     /// entry.attach_one(Name("Hello, World"));
-    /// assert!(entry.attached_one::<Name>());
+    ///
+    /// let name = entry.get::<Name>().unwrap();
+    /// assert_eq!(*name, Name("Hello, World"));
     /// ```
-    pub fn attached_one<C>(&self) -> bool
+    pub fn get<C>(&'r self) -> Option<Ref<'r, C>>
     where
         C: Component,
     {
-        self.registry.attached_one::<C>(self.entity)
+        self.registry.get(self.entity)
     }
 
-    /// Returns `true` if components in the generic set type are attached to the entity.
+    /// Retrieves the [unique borrow][`RefMut`] for the component of one type attached to the entity.
+    /// Returns [`None`][`Option::None`] if component is not attached to the entity.
     ///
-    /// To check if the entity has component of exactly one type,
-    /// use [`attached_one`][`Entry::attached_one`] associated function.
+    /// Note that function would block current thread
+    /// if the same instance of component will be retrieved more than once.
     ///
     /// # Examples
     ///
     /// ```
     /// # use toucan_ecs::{Registry, Entry};
     /// # let mut registry = Registry::new();
-    /// #[derive(Copy, Clone)]
+    /// #[derive(Copy, Clone, Eq, PartialEq, Debug)]
     /// struct Name(&'static str);
     ///
-    /// #[derive(Copy, Clone)]
-    /// struct ID(u32);
-    ///
     /// let mut entry = registry.create_entry();
-    /// assert!(!entry.attached::<(ID, Name)>());
+    /// entry.attach_one(Name("Hello, World"));
     ///
-    /// entry.attach((Name("Hello, World"), ID(42)));
-    /// assert!(entry.attached::<(Name, ID)>());
+    /// let mut name = entry.get_mut::<Name>().unwrap();
+    /// (*name).0 = "This name was changed";
+    /// assert_ne!(*name, Name("Hello, World"));
+    /// assert_eq!(*name, Name("This name was changed"));
     /// ```
-    pub fn attached<S>(&self) -> bool
+    pub fn get_mut<C>(&'r mut self) -> Option<RefMut<'r, C>>
     where
-        S: ComponentSet,
+        C: Component,
     {
-        self.registry.attached::<S>(self.entity)
-    }
-
-    /// Destroys the entity and removes all its attached components.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use toucan_ecs::{Registry, Entry};
-    /// # let mut registry = Registry::new();
-    /// let mut entry = registry.create_entry();
-    /// let entity = entry.entity();
-    ///
-    /// entry.destroy();
-    /// assert!(!registry.contains(entity));
-    /// ```
-    pub fn destroy(self) {
-        self.registry.destroy(self.entity)
-    }
-
-    /// Returns unique handle of the entity.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use toucan_ecs::{Registry, Entry};
-    /// # let mut registry = Registry::new();
-    /// let mut entry = registry.create_entry();
-    /// let entity = entry.entity();
-    /// assert!(registry.contains(entity));
-    /// ```
-    pub fn entity(&self) -> Entity {
-        self.entity
+        self.registry.get_mut(self.entity)
     }
 }
