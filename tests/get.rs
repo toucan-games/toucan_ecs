fe85b78b7832cbe5525d1b01317a43bec25f467d
tuguzT <timurka.tugushev@gmail.com>
2022-03-20 11:56:09 +0300

    Add new test to check actual mutation

---
+++ tests/get.rs
@@ -0,0 +1,37 @@
+use toucan_ecs::World;
+use components::Velocity;
+
+mod components;
+
+#[test]
+fn get() {
+    let mut world = World::new();
+    let entity = world.create_with_one(Velocity { dx: 10.0, dy: -10.0 });
+
+    let data = world.get::<Velocity>(entity).unwrap();
+    assert_eq!(*data, Velocity { dx: 10.0, dy: -10.0 });
+}
+
+#[test]
+fn get_mut() {
+    let mut world = World::new();
+    let entity = world.create_with_one(Velocity { dx: 10.0, dy: -10.0 });
+
+    let data = world.get_mut::<Velocity>(entity).unwrap();
+    assert_eq!(*data, Velocity { dx: 10.0, dy: -10.0 });
+}
+
+#[test]
+fn get_mut_and_mutate() {
+    let mut world = World::new();
+    let entity = world.create_with_one(Velocity { dx: 10.0, dy: -10.0 });
+
+    let mut data = world.get_mut::<Velocity>(entity).unwrap();
+    data.dx = 0.0;
+    data.dy = 0.0;
+    assert_eq!(*data, Velocity { dx: 0.0, dy: 0.0 });
+
+    let data_copy = *data;
+    drop(data);
+    assert_eq!(data_copy, *world.get::<Velocity>(entity).unwrap());
+}
