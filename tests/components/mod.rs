c7c9062378cfbd20b7f434ea2283973db06173dd
tuguzT <timurka.tugushev@gmail.com>
2022-07-15 10:35:59 +0300

    Derive macro: add support for generic components

--- tests/components/mod.rs
+++ tests/components/mod.rs
@@ -14,3 +14,8 @@ pub struct Velocity {
 
 #[derive(Copy, Clone, Component, Debug, PartialEq)]
 pub struct Mass(pub f32);
+
+#[derive(Copy, Clone, Component, Debug, PartialEq)]
+struct Generic<T>(pub T)
+where
+    T: Component;
--- toucan_ecs_derive/src/lib.rs
+++ toucan_ecs_derive/src/lib.rs
@@ -15,14 +15,18 @@ struct Options {
 pub fn component_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input);
     let Options { storage } = Options::from_derive_input(&input).expect("Wrong storage format");
-    let DeriveInput { ident, .. } = input;
+    let DeriveInput {
+        ident, generics, ..
+    } = input;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
+    let trait_ident = quote! { toucan_ecs::component::Component };
     let storage = match &storage[..] {
         &[] => quote! { toucan_ecs::component::storage::DefaultStorage<Self> },
         storage => quote! { #( #storage )* },
     };
     let output = quote! {
-        impl toucan_ecs::component::Component for #ident {
+        impl #impl_generics #trait_ident for #ident #ty_generics #where_clause {
             type Storage = #storage;
         }
     };
